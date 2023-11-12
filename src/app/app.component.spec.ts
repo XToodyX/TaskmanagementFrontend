41202ddae62e905ac162d5ad0396a37da9f815b4
dominicherrmann <dherrmann.oas@gmail.com>
2023-11-12 19:10:24 +0100

    :package: Added tailwindcss

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'TaskmanagementFrontend' title`, () => {
+  it('should have the TaskmanagementFrontend title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('TaskmanagementFrontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TaskmanagementFrontend');
   });
 });
